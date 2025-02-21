861c9d481688a4fc9921859d6da66e1195d7200f
fery-blg <ferielblg5@gmail.com>
2025-02-21 11:35:45 +0100

    struct etudient

--- ex.c
+++ ex.c
@@ -1,57 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+
 struct Point {
     float x;
     float y;
 };
 
-void  saisie1(){
-    struct Point PA;
-    printf ("Donnez X:");
-    scanf ("%f", &PA.x);
-    printf ("Donnez Y:");
-    scanf ("%f", &PA.y);
+void saisie(struct Point *p) {
+    printf("Donnez X: ");
+    scanf("%f", &p->x);
+    printf("Donnez Y: ");
+    scanf("%f", &p->y);
 }
 
-void saisie2(){
-    struct Point PB;
-    printf ("Donnez X:");
-    scanf ("%f", &PB.x);
-    printf ("Donnez Y:");
-    scanf ("%f", &PB.y);
+void affichage_point(struct Point p) {
+    printf("Les coordonnées du point sont : %f %f\n", p.x, p.y);
 }
 
-void Affichage_point1(){
-    struct Point PA ;
-    printf("Les coordonnées du point sont : %f %f", PA.x, PA.y);
-   
-
-}
-void Affichage_point2(){
-    struct Point PB ;
-    printf("Les coordonnées du point sont : %f %f", PB.x, PB.y);
-   
-
-}
-float distance () {
-    struct Point PA ;
-    struct Point PB ;
-    float d;
-    
-    d = sqrt((PA.x - PB.x)*(PA.x - PB.x) + (PA.y - PB.y)*(PA.y - PB.y));
-    printf("La distance entre les deux points est : %f", d);
-
-
+float distance(struct Point p1, struct Point p2) {
+    return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 }
 
+int main() {
+    struct Point PA, PB;
 
+    saisie(&PA);
+    saisie(&PB);    
+    affichage_point(PA);
+    affichage_point(PB);
+    distance(p1:PA,p2: PB);
 
-
-
-int main (){
-    saisie1();
-    saisie2();
-    Affichage_point1();
-    Affichage_point2();
-    distance();
 }
